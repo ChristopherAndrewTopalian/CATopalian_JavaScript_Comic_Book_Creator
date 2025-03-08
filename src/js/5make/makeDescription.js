9e1a0825e2d436a9aced018b04f8feee3d4a4b39
[Christopher Andrew Topalian] <[scriptingdata@gmail.com]>
2025-03-08 11:04:45 -0500

    update

--- src/js/5make/makeDescription.js
+++ src/js/5make/makeDescription.js
@@ -7,27 +7,53 @@ function makeDescription(whichArray)
     let mainDiv = ce("div");
     mainDiv.id = theId;
     mainDiv.style.position = "fixed";
-    mainDiv.style.right = 15 + "px";
-    mainDiv.style.bottom = 70 + "px";
-    mainDiv.style.paddingLeft = 12 + "px";
-    mainDiv.style.paddingRight = 12 + "px";
-    mainDiv.style.paddingTop = 3 + "px";
-    mainDiv.style.paddingBottom = 3 + "px";
-    mainDiv.style.borderRadius = 8 + "px";
+    mainDiv.style.right = "15px";
+    mainDiv.style.bottom = "70px";
+    mainDiv.style.paddingLeft = "12px";
+    mainDiv.style.paddingRight = "12px";
+    mainDiv.style.paddingTop = "3px";
+    mainDiv.style.paddingBottom = "3px";
+    mainDiv.style.borderRadius = "8px";
     mainDiv.style.zIndex = 900;
-    setTimeout(function()
-    {
-        if (ge(theId))
-        {
-            ge(theId).remove();
-        }
-    }, 2000);
-
     mainDiv.style.backgroundColor = "black";
     mainDiv.className = "menuStyle";
-    mainDiv.innerHTML = whichArray[0].sceneTitle.bold() + "<br>" + whichArray[0].scene + "<hr>" + whichArray[0].description;
     mainDiv.style.color = "rgb(255, 255, 255)";
     ba(mainDiv);
+
+    //-//
+
+    let theTitle = ce("div");
+    theTitle.innerHTML = whichArray[0].sceneTitle.bold();
+    mainDiv.append(theTitle);
+
+    //-//
+
+    let scene = ce("div");
+    scene.textContent = whichArray[0].scene;
+    mainDiv.append(scene);
+
+    //-//
+
+    mainDiv.append(ce('hr'));
+
+    //-//
+
+    let description = ce("div");
+    description.textContent = whichArray[0].description;
+    mainDiv.append(description);
+
+    //-//
+
+    // remove mainDiv after 2 seconds
+    setTimeout(function()
+    {
+        let existingDiv = ge(theId);
+
+        if (existingDiv)
+        {
+            existingDiv.remove();
+        }
+    }, 2000);
 }
 
 //----//
