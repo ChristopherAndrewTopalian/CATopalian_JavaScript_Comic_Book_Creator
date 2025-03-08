cc6ca6524f2ed4bbef23d84c69016cef6cee9ce1
[Christopher Andrew Topalian] <[scriptingdata@gmail.com]>
2025-03-08 13:40:40 -0500

    update

--- src/js/video/backgroundMovieCreateSize.js
+++ src/js/video/backgroundMovieCreateSize.js
@@ -2,68 +2,41 @@
 
 function backgroundMovieCreateSize(whichArray, whichItem)
 {
-    /*
-    if (document.getElementsByClassName("theBackground2")) // theBackgroundMovieHere
+    if (ge('videoMainDiv'))
     {
-        eraseDivs("backgroundMoviesStyle");
-        eraseDivs("theBackgroundMovie");
-        eraseDivs("theBackground2");
+        ge('videoMainDiv').remove();
     }
-    */
 
-    let whichFolder = whichArray[whichItem].theUrl;
+    let mainDiv = ce('div');
+    mainDiv.id = 'videoMainDiv';
+    mainDiv.style.position = 'absolute';
+    mainDiv.style.left = whichArray[whichItem].theX + "px";
+    mainDiv.style.top = whichArray[whichItem].theY + "px";
+    mainDiv.style.width = whichArray[whichItem].sizeX + 'px';
+    ba(mainDiv);
 
-    let theMovie = ce("div");
-    theMovie.id = "theBackgroundMovie";
-    theMovie.title = 'theBackgroundMovie';
-    theMovie.className = "backgroundMoviesStyle";
-
-    theMovie.style.position = "absolute";
-    //theMovie.style.left = 0 + "px";
-    //theMovie.style.top = 0 + "px";
-    
-    theMovie.style.left = whichArray[whichItem].theX + "px";
-    theMovie.style.top = whichArray[whichItem].theY + "px";
-
-    //theMovie.style.width = whichArray[whichItem].sizeX + "px";
-
-    theMovie.setAttribute('width', whichArray[whichItem].sizeX + "px");
-
-    theMovie.style.zIndex = whichArray[whichItem].theLayer;
-    ba(theMovie);
+    makeElementDraggable(mainDiv);
 
     //-//
 
-    theMovie.innerHTML = '<video loop id =\''+whichArray[whichItem].theId+'\'" autoplay muted height = \''+whichArray[whichItem].sizeX+'\' style = \'position: absolute;\'><source src= \''+whichFolder+'\' type="video\/mp4"><\/video> ';
-
-    theMovie.oncontextmenu = function()
-    {
-        playSound(sfx[3], 0.1);
-
-        menuOptions(whichArray[whichItem].theId);
-
-        idToMove = whichArray[whichItem].theId;
-    };
-
-    theMovie.onclick = function()
-    {
-        moveElement(idToMove);
-    };
+    let videoElement = ce('video');
+    videoElement.id = 'theVideo';
+    videoElement.setAttribute('height', whichArray[whichItem].sizeX + 'px');
+    videoElement.setAttribute('loop', true);
+    videoElement.setAttribute('autoplay', true);
+    //videoElement.setAttribute('muted', true);
+    videoElement.muted = true;
+    mainDiv.append(videoElement);
 
     //-//
 
-    theMovie.onmousedown = function()
-    {
-        idToMove = whichArray[whichItem].theId;
-    };
+    let sourceElement = ce('source');
+    sourceElement.setAttribute('src', whichArray[whichItem].theUrl);
+    sourceElement.setAttribute('type', 'video/mp4');
+    videoElement.append(sourceElement);
 
-    if (whichArray[whichItem].rotationAmount != null)
-    {
-        theMovie.style.rotate = whichArray[whichItem].rotationAmount;
-    }
-    ba(theMovie);
-
-    makeElementDraggable(theMovie);
+    // load video 
+    // videoElement.load();
 }
 
 //----//
