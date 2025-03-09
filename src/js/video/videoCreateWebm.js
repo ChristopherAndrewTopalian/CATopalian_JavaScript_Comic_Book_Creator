721978e71780fc83034fa0a89a1365ea70aad0d9
[Christopher Andrew Topalian] <[scriptingdata@gmail.com]>
2025-03-09 12:47:39 -0400

    update

--- src/js/video/videoCreateWebm.js
+++ src/js/video/videoCreateWebm.js
@@ -22,26 +22,14 @@ function videoCreateWebm(whichVideo, whichNumber, whichId, theX, theY, sizeX, th
     theSource.src = whichVideo;
     theSource.setAttribute("type", "video/mp4");
 
-    //theImage.src = "src/media/texture/prop/" + whichVideo + "\/" + whichVideo + "_" + whichNumber + ".webp";
-
-    //theImage.src = "src/media/texture/prop/" + whichVideo + "_" + whichNumber + ".webp";
-
-    //theImage.src = "video/" + whichVideo;
-
-    //theImage.id = whichId;
-
-    //theVideo.className = whichClass;
-
     theVideo.style.position = "absolute";
     theVideo.style.left = theX + "px";
     theVideo.style.top = theY + "px";
     theVideo.style.width = sizeX + "px";
     theVideo.style.zIndex = 200000;
     theVideo.append(theSource);
-    //theVideoDiv.style.backgroundSize = "cover";
-    //theVideoDiv.style.zIndex = theZindex;
-    //theVideoDiv.innerHTML = '<video width="800" height = "30%" controls id =\''+whichId+'\' style = "background-size: cover;"><source src= \''+whichVideo+'\' type="video\/mp4"><\/video> ';
-    //ba(theVideoDiv);
+
+    //-//
 
     ge(whichId).oncontextmenu = function()
     {
