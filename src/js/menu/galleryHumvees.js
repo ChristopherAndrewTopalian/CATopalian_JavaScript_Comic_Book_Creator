4dba64e53f247716b935e0e8208fac95f21152bd
[Christopher Andrew Topalian] <[scriptingdata@gmail.com]>
2025-03-14 22:01:07 -0400

    update

--- src/js/menu/galleryHumvees.js
+++ src/js/menu/galleryHumvees.js
@@ -212,6 +212,36 @@ let galleryHumvees =
     theDropShadowColor: "black",
 },
 
+{
+    name: "humvee_008",
+    texture: "src/media/texture/prop/humvee/humvee_008.webp",
+    theId: "humvee_008",
+    category: "humvee",
+    country: "America",
+    sizeX: 256,
+    posX: 0,
+    posY: 20,
+    type: "webp",
+    theLayer: 5,
+    theOpacity: 1,
+    flipHorizontal: false,
+    flipVertical: false,
+    className: "propStyle",
+    rotationAmount: 0 + "deg",
+    theBrightness: 1.0,
+    theContrast: 1.0,
+    theHue: 0,
+    theSaturation: 1.0,
+    theBlur: 0,
+    theInvert: 0,
+    theGrayscale: 0,
+    theSepia: 0,
+    theDropShadowX: "10px",
+    theDropShadowY: "10px",
+    theDropShadowBlurRadius: "10px",
+    theDropShadowColor: "black",
+},
+
 ];
 
 //----//
