87e38526c471f53e4cb269ad36a6aa8d84489094
[Christopher Andrew Topalian] <[scriptingdata@gmail.com]>
2025-03-14 21:17:27 -0400

    update

--- src/js/menu/galleryTanks.js
+++ src/js/menu/galleryTanks.js
@@ -213,6 +213,36 @@ let galleryTanks = [
     theDropShadowColor: "black",
 },
 
+{
+    name: "tank_004",
+    texture: "src/media/texture/prop/tank/tank_004.webp",
+    theId: "tank_004",
+    category: "tank",
+    country: "America",
+    sizeX: 200,
+    posX: 0,
+    posY: 20,
+    type: "webp",
+    theLayer: 5,
+    theOpacity: 1,
+    flipHorizontal: false,
+    flipVertical: false,
+    className: "propStyle",
+    rotationAmount: 0 + "deg",
+    theBrightness: 1.0,
+    theContrast: 1.0,
+    theHue: 0,
+    theSaturation: 1.0,
+    theBlur: 0,
+    theInvert: 0,
+    theGrayscale: 0,
+    theSepia: 0,
+    theDropShadowX: "10px",
+    theDropShadowY: "10px",
+    theDropShadowBlurRadius: "10px",
+    theDropShadowColor: "black",
+},
+
 ];
 
 //----//
