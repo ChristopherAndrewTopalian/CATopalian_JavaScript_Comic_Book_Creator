224951a62be721c95101276b1addd52b832d750d
Christopher Andrew Topalian <scriptingdata@gmail.com>
2025-11-22 13:29:11 -0500

    update

--- src/js/scenes/circuits/scene_led_circuit_001.js
+++ src/js/scenes/circuits/scene_led_circuit_001.js
@@ -206,3 +206,12 @@ let scene_led_circuit_001 = [
 },
 
 ];
+
+//----//
+
+// Dedicated to God the Father
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2025
+// https://github.com/ChristopherTopalian
+// https://github.com/ChristopherAndrewTopalian
+// https://sites.google.com/view/CollegeOfScripting
+
